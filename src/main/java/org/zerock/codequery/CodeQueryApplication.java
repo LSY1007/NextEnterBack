80063b4886956d73627e4c51000202fe06855e46
LSY1007 <dltkddus50@naver.com>
2026-01-05 14:18:50 +0900

    백앤드 프로젝트 기본 구조 생성.

---
+++ src/main/java/org/zerock/codequery/CodeQueryApplication.java
@@ -0,0 +1,13 @@
+package org.zerock.codequery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeQueryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeQueryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/zerock/codequery/CodeQueryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zerock.codequery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeQueryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
